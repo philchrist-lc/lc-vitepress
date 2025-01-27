8644ad21ec03757782417f1aa8e88d76b7bd152d
Philipp Christ <pc@lc-top.de>
2025-01-27 16:48:22 +0100

    Git Workflow

---
+++ docs/.vitepress/config.js
@@ -0,0 +1,5 @@
+export default {
+    base: '/my-vitepress-site/', // Repository-Name (falls es kein root-Repository ist)
+    title: 'Mein Vitepress-Projekt',
+    description: 'Eine Beschreibung meiner Vitepress-Seite',
+}
